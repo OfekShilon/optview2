8d4afd317ba883cc095e3f6ba13d7eb160c97cad
Ilan Ben Hagai <supox0@walla.co.il>
2021-10-20 16:25:03 +0300

    cpp_optimization_example: better example - aliasing issue

--- cpp_optimization_example/main.cc
+++ cpp_optimization_example/main.cc
@@ -1,8 +1,21 @@
 #include <iostream>
-#include "./another.hh"
+#include <vector>
+
+// Based on Roi Barkan - "Argument passing, core guidelines and concepts" - https://www.youtube.com/watch?v=uylFACqcWYI
+void scale_down(std::vector<double>& v, const double& a) {
+    for (auto& item : v) {
+        item /= a;
+    }
+}
+
+void scale_down_example() {
+    std::vector<double> v {2, 1, 2, 3, 4};
+    scale_down(v, v[0]);
+
+}
 
 int main() {
-    Another a(3);
-    std::cout << "Value of a is " << a.getValue() << std::endl;
+    scale_down_example();
+
     return 0;
 }
